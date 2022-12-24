86760f8b48d3707387bd5415536f84a2972d88d1
polrmn <polrmn@gmail.com>
2022-12-24 22:54:49 +0200

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,10 +8,6 @@ player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.on('play', function () {
-  console.log('played the video!'); 
-}); 
-
 function onPlay(data) {
      console.log(data.seconds);
      localStorage.setItem('videoplayer-current-time', data.seconds);
