14ca7e7962c7c7dba4efb3e7e6c287cc42e703fc
polrmn <114829451+polrmn@users.noreply.github.com>
2022-12-24 21:56:14 +0000

    Deploying to gh-pages from @ polrmn/goit-js-hw-08@b87fddf932cfaae3298a1260873fb72d7e491441 🚀

---
+++ 03-feedback.37302bb6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form"),t={};e.addEventListener("input",(function(e){t[e.target.name]=e.target.value,console.log(t),localStorage.setItem("feedback-form-state",JSON.stringify(t))}))}();
+//# sourceMappingURL=03-feedback.37302bb6.js.map
